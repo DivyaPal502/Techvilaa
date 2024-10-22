c1d9e81beef1209735f3b42e3cc687bd25144297
DivyaPal502 <divyapal7575@gmail.com>
2024-10-22 17:11:04 +0530

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <footer>
+        <div>
+        <h1>TECHVILLA</h1>
+        <p>©All rights reserved</p>
+        </div>
+
+        <div>
+<h2>Follow Us</h2>
+<br></br>
+<a href="https://www.linkedin.com/in/divya-pal-11a8a7210/">LinkedIn</a>
+<a href="#">Github</a>
+<a href="#">Instagram</a>
+        </div>
+        </footer>
+    )
+}
+
+export default Footer;
